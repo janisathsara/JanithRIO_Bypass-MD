d6df45031bd2abc01ac8875aa82c83ba1ec52ddc
janisathsara <sathsarajani8@gmail.com>
2025-04-25 19:34:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,5 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=e3AwzRiY#250RFZcaHvI2DYZoItrl3SwNUVbufXhMBv0fRlEfSwo' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
-POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POapavhaaSQL_URL,
+POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POapavaSQL_URL,
 };
